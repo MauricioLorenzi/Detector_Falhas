5af697a8734a95fbc5f6235bf0dacc60aa574902
Mauricio Luis de Lorenzi <luis_mauricio_13_@hotmail.com>
2018-09-12 14:43:41 -0300

    fixed some bugs, ready to implement the leader election algorithm

--- DetectorFalhas.py
+++ DetectorFalhas.py
@@ -1,57 +1,71 @@
+import os
 import socket
 import _thread
 import time
 
 MESSAGE = b"1"
 MESSAGE_2 = b"2"
-UDP_IP = "127.0.0.1"
-UDP_SEND_PORT = 6002
+UDP_IP = "172.18.3.100"
+UDP_SEND_PORT = 6000
 UDP_RECEIVE_PORT = 6001
 
-#all_proc = ["172.18.2.188", "172.18.1.52", "172.18.3.181", "172.18.1.69", "172.18.1.69", "172.18.1.64"]
-all_proc = ["127.0.0.2"]
+SEND_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+SEND_SOCKET.bind((UDP_IP, UDP_SEND_PORT))
+
+LISTEN_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+LISTEN_SOCKET.bind((UDP_IP, UDP_RECEIVE_PORT))
+
+all_proc = ["127.0.0.1", "127.0.0.2"]
 mutex = _thread.allocate_lock()
-alive = ["127.0.0.2"]
+alive = ["127.0.0.1", "127.0.0.2"]
 detected = []
+leader = None
+
+clear = lambda: os.system('cls')
 
 def listen():
-    sock_listen = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock_listen.bind(("", UDP_RECEIVE_PORT))
+    message_received, ip_sender = LISTEN_SOCKET.recvfrom(1024)
+    ip_sender = ip_sender[0]
 
-    message_received, ip_sender = sock_listen.recvfrom(1024)
-    print("\nSender: {} => Mensagem recebida: {}".format(ip_sender, message_received))
+    message_received = ord(message_received)
     
-    if message_received is MESSAGE:                        
-        oldtime = time.time()
+    if message_received != UDP_IP:    
+        print("\nMensagem recebida => {} enviado por => {}".format(str(message_received), ip_sender))
+    
+    if message_received is 1:
+        response(ip_sender)        
 
-        while time.time() - oldtime < 3:
-            sock_listen.bind(("", UDP_RECEIVE_PORT))
-            message_received_2, ip_sender_2 = sock_listen.recvfrom(1024)
+    if message_received is 2:
+        mutex = _thread.allocate_lock()
+        with mutex:                    
+            mutex.acquire()
+            if ip_sender not in alive:
+                alive.append(ip_sender)
+            mutex.release()
 
-        if message_received_2 is MESSAGE_2:
-            mutex = _thread.allocate_lock()
-            with mutex:                    
-                mutex.acquire()
-                alive.append(ip_sender_2)
-                mutex.release()
+def response(ip):   
+    try:
+        SEND_SOCKET.sendto(MESSAGE, (ip, UDP_SEND_PORT))
+    except:
+        if ip not in alive and ip not in detected:
+            detected.append(ip)
+            sendException(ip)
 
-def response(ip):    
-    sock_response = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock_response.settimeout(5)
-    sock_response.sendto(MESSAGE, (UDP_IP, UDP_SEND_PORT))
-
-def sendException(p):
-    print("\nprocesso {} esta com falha".format(p))
+def sendException(ip):
+    print("\nprocesso {} esta com falha".format(ip))
 
 def thread_send():
     while True:            
         for p in all_proc:
-            if p not in alive and p not in detected:
-                detected.append(p)
-                sendException(p)
-            time.sleep(10)
             response(p)
-        alive.clear()
+        time.sleep(3)
+
+        mutex = _thread.allocate_lock()
+
+        with mutex:                    
+            mutex.acquire()
+            alive.clear()
+            mutex.release()
 
 def thread_listen():    
     while True:
@@ -59,9 +73,15 @@ def thread_listen():
 
 def thread_print():
     while True:
-        time.sleep(10)        
-        print("\nVivos => {}".format(alive))
-        print("\nDetectados => {}".format(detected))
+        time.sleep(5)
+        clear()
+        #with mutex:                    
+         #   mutex.acquire()
+        print("Todos => {}".format(all_proc))       
+        print("Vivos => {}".format(alive))
+        print("Mortos => {}".format(detected))
+        print("Lider => {} \n".format(leader))
+          #  mutex.release()
 
 time.sleep(5)
 _thread.start_new_thread(thread_listen, ())
