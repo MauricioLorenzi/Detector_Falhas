948ebcd158370a3b1dc6c3de267b70c476d5553d
Mauricio Luis de Lorenzi <luis_mauricio_13_@hotmail.com>
2018-09-14 19:57:48 -0300

    leader election done, cleaned the code and its ready to submit

--- DetectorFalhas.py
+++ DetectorFalhas.py
@@ -3,8 +3,12 @@ import socket
 import _thread
 import time
 
-MESSAGE = b"1"
-MESSAGE_2 = b"2"
+MESSAGE = bytearray()
+MESSAGE_2 = bytearray()
+
+MESSAGE.append(1)
+MESSAGE_2.append(2)
+
 UDP_IP = "172.18.3.100"
 UDP_SEND_PORT = 6000
 UDP_RECEIVE_PORT = 6001
@@ -15,78 +19,61 @@ SEND_SOCKET.bind((UDP_IP, UDP_SEND_PORT))
 LISTEN_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 LISTEN_SOCKET.bind((UDP_IP, UDP_RECEIVE_PORT))
 
-all_proc = ["127.0.0.1", "127.0.0.2"]
-mutex = _thread.allocate_lock()
-alive = ["127.0.0.1", "127.0.0.2"]
-detected = []
-leader = None
+ALL_IPS = ["172.18.1.41", "172.18.2.209", "172.18.3.100", "172.17.98.96", "172.18.1.52"]
+MUTEX = _thread.allocate_lock()
+ALIVE = ["172.18.1.41", "172.18.2.209", "172.18.3.100", "172.17.98.96", "172.18.1.52"]
+DETECTED = []
+LEADER = ""
 
 clear = lambda: os.system('cls')
 
-def listen():
-    message_received, ip_sender = LISTEN_SOCKET.recvfrom(1024)
-    ip_sender = ip_sender[0]
-
-    message_received = ord(message_received)
-    
-    if message_received != UDP_IP:    
-        print("\nMensagem recebida => {} enviado por => {}".format(str(message_received), ip_sender))
-    
-    if message_received is 1:
-        response(ip_sender)        
-
-    if message_received is 2:
-        mutex = _thread.allocate_lock()
-        with mutex:                    
-            mutex.acquire()
-            if ip_sender not in alive:
-                alive.append(ip_sender)
-            mutex.release()
-
-def response(ip):   
-    try:
-        SEND_SOCKET.sendto(MESSAGE, (ip, UDP_SEND_PORT))
-    except:
-        if ip not in alive and ip not in detected:
-            detected.append(ip)
-            sendException(ip)
-
-def sendException(ip):
-    print("\nprocesso {} esta com falha".format(ip))
-
 def thread_send():
+    global LEADER
     while True:            
-        for p in all_proc:
-            response(p)
-        time.sleep(3)
+        for p in ALL_IPS:
+            try:
+                SEND_SOCKET.sendto(MESSAGE, (p, UDP_RECEIVE_PORT))
+            except:
+                if p not in detected:
+                    DETECTED.append(p)
+        time.sleep(2)        
 
-        mutex = _thread.allocate_lock()
-
-        with mutex:                    
-            mutex.acquire()
-            alive.clear()
-            mutex.release()
+        if LEADER not in ALIVE:
+            for ip in ALL_IPS:
+                if ip in ALIVE:
+                    LEADER = ip
+                    break
+                 
+        MUTEX.acquire()
+        for ip in ALL_IPS:
+            if ip not in ALIVE and ip not in DETECTED:
+                DETECTED.append(ip)
+        clear()
+        print("Todos Ips => {}".format(ALL_IPS))
+        print("Vivos => {}".format(ALIVE))
+        print("Mortos => {}".format(DETECTED))
+        print("Lider => {} \n".format(LEADER))
+        ALIVE.clear()
+        MUTEX.release()                                        
 
 def thread_listen():    
     while True:
-        listen()
+        message_received, ip_sender = LISTEN_SOCKET.recvfrom(1024)
+        ip_sender = ip_sender[0]
+        message_received = message_received[0]
+        
+        if message_received != UDP_IP:    
+            print("\nMensagem recebida => {} enviado por => {}".format(str(message_received), ip_sender))
+        
+        if message_received == 1:
+            SEND_SOCKET.sendto(MESSAGE_2, (ip_sender, UDP_RECEIVE_PORT))       
 
-def thread_print():
-    while True:
-        time.sleep(5)
-        clear()
-        #with mutex:                    
-         #   mutex.acquire()
-        print("Todos => {}".format(all_proc))       
-        print("Vivos => {}".format(alive))
-        print("Mortos => {}".format(detected))
-        print("Lider => {} \n".format(leader))
-          #  mutex.release()
+        elif message_received == 2:                
+            if ip_sender not in ALIVE and not MUTEX.locked():
+                ALIVE.append(ip_sender)
 
-time.sleep(5)
-_thread.start_new_thread(thread_listen, ())
 _thread.start_new_thread(thread_send, ())
-_thread.start_new_thread(thread_print, ())       
+_thread.start_new_thread(thread_listen, ())
 
 while True:
     pass
